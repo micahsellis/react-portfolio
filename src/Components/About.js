3d7336323596681e3f023983a30a38160095a53b
Micah Ellis <micahsellis@gmail.com>
2022-06-05 22:06:22 -0600

    add links to about me

--- src/Components/About.js
+++ src/Components/About.js
@@ -44,7 +44,10 @@ class About extends Component {
                <div className="columns download">
                   <p>
                          <a target="_blank" rel="noopener noreferrer" href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
+                      </p>
+                      <ul className="social">
+                         {networks}
+                      </ul>
                </div>
                 </div>
          </div>
