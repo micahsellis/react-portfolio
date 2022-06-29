8d11a232fc122e99c0e8147094a3f3f973f7b854
Micah Ellis <micahsellis@gmail.com>
2022-06-29 16:46:37 -0600

    Updates

---
+++ precache-manifest.5de60eed35551922e970764a29fa859a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b7c1124a2d223b69cef86cdef2a8ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cb03f9d9638ac9bef53",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "45d8dd612cc18ee6e5dd",
+    "url": "/static/js/2.0edd63f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0edd63f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb03f9d9638ac9bef53",
+    "url": "/static/js/main.d464ecff.chunk.js"
+  },
+  {
+    "revision": "48c9281eb11a3097ac6d",
+    "url": "/static/js/runtime-main.c50c390d.js"
+  }
+]);
