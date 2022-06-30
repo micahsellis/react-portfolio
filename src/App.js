f39af38267b85e824ddce8e4bd5b441a545d2db9
Micah Ellis <micahsellis@gmail.com>
2022-06-29 18:12:51 -0600

    deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
       foo: 'bar',
@@ -19,7 +19,7 @@ class App extends Component {
 
   }
 
-  getResumeData() {
+  getResumeData(){
     this.setState({
       resumeData:
       {
@@ -160,20 +160,20 @@ class App extends Component {
 
   }
 
-  componentDidMount() {
+  componentDidMount(){
     this.getResumeData();
   }
 
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main} />
-        <About data={this.state.resumeData.main} />
-        <Portfolio data={this.state.resumeData.portfolio} />
-        <Resume data={this.state.resumeData.resume} />
-        <Testimonials data={this.state.resumeData.testimonials} />
-        <Contact data={this.state.resumeData.main} />
-        <Footer data={this.state.resumeData.main} />
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
+        <Contact data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/>
       </div>
     );
   }
