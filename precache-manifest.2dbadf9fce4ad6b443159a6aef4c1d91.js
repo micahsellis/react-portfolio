32d274355b5bda99ff26e5d0e2b43b776ff13583
Micah <micahsellis@gmail.com>
2024-08-20 23:22:01 -0600

    Updates

---
+++ precache-manifest.2dbadf9fce4ad6b443159a6aef4c1d91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caedc78d2fecb38eb0bcdeac9b4b2466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2fb3709efeaef1002ee",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "45d8dd612cc18ee6e5dd",
+    "url": "/static/js/2.0edd63f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0edd63f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2fb3709efeaef1002ee",
+    "url": "/static/js/main.85cc5666.chunk.js"
+  },
+  {
+    "revision": "48c9281eb11a3097ac6d",
+    "url": "/static/js/runtime-main.c50c390d.js"
+  }
+]);
